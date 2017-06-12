9f9cfe1fda5743ebbd208b66d8bd8871b9811784
liweiqing <2234660612@qq.com>
2017-06-12 22:24:33 +0800

    Initial commit

---
+++ app/src/main/java/lwq/com/toastdemo/MainActivity.java
@@ -0,0 +1,51 @@
+package lwq.com.toastdemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_toast)
+    Button btnToast;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mContext =this;
+    }
+
+    @OnClick(R.id.btn_toast)
+    public void onViewClicked() {
+        midToast("提莫队长，正在送命~");
+    }
+
+    private void midToast(String str) {
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.view_toast_custom, (ViewGroup) findViewById(R.id.lly_toast));
+        ImageView img_logo = (ImageView) view.findViewById(R.id.img_logo);
+        TextView tv_msg = (TextView) view.findViewById(R.id.tv_msg);
+        tv_msg.setText(str);
+        Toast toast = new Toast(mContext);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+    }
+}
